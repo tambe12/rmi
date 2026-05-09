a69fdf5ce817aba7adbde321785d76c068e30b0b
tambe12 <clavecris16@gmail.com>
2026-05-09 18:27:52 +0530

    Update AddClient.java

--- AddClient.java
+++ AddClient.java
@@ -35,3 +35,53 @@ public class AddClient {
 // to change the version from eclips, navigate to project->properties->java_compiler and change to appropriate version
 // the go to  project->properties->java_compiler. go to libraries path and delete older librabry and add new labrary with proper version.
 // Make sure to delete the module-info.java file from the project. 
+
+package ds_2;
+
+import java.rmi.*;
+
+public class AddServer {
+	public static void main(String[] args) {
+		try {
+			AddServerImpl addoAddServerImpl = new AddServerImpl();
+			
+			Naming.rebind("AddServer", addoAddServerImpl);
+			
+			System.out.println("Server Started...");
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+///////////////////////
+package ds_2;
+
+import java.rmi.*;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+
+public class AddServerImpl extends UnicastRemoteObject implements AddServerIntf {
+	public AddServerImpl() throws RemoteException {
+		super();
+	}
+	
+	public double add(double num1, double num2) throws RemoteException{
+		System.out.println("Processing request in thread : "+ Thread.currentThread().getName());
+		
+		return num1 + num2;
+	}
+
+}
+
+//////////////////////
+
+package ds_2;
+
+import java.rmi.*;
+import java.rmi.RemoteException;
+
+public interface AddServerIntf extends Remote {
+	double add(double num1, double num2)throws RemoteException;
+}
